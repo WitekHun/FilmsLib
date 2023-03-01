44b642e9d27ebca689b71ac919f999647207c89e
Witold Hungendorfer <witekh@me.com>
2023-03-01 20:09:34 +0100

    Change arguments in functions

--- films_lib.py
+++ films_lib.py
@@ -1,3 +1,8 @@
+import random
+from datetime import date
+
+today = date.today()
+
 
 class Films:
     def __init__(self, title, year, genre):
@@ -9,10 +14,16 @@ class Films:
         self._views = 0
 
     def __hash__(self):
-        return hash(self.title)+hash(self.year)+hash(self.genre)
+        return hash(self.title) + hash(self.year) + hash(self.genre)
 
     def __eq__(self, other):
-        return all((self.title == other.title, self.year == other.year, self.genre == other.genre))
+        return all(
+            (
+                self.title == other.title,
+                self.year == other.year,
+                self.genre == other.genre,
+            )
+        )
 
     def __repr__(self):
         return f"{self}"
@@ -35,13 +46,6 @@ class Films:
         self._views += value
 
 
-"""
-class Movies(Films):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-"""
-
-
 class Series(Films):
     def __init__(self, episode, season, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -51,112 +55,109 @@ class Series(Films):
     def __str__(self):
         return f'"{self.title}" S{self.season:02d}E{self.episode:02d} ({self.year})'
 
-# hash dodany po zdefiniowaniu __eq__ domyślny nie działał, potrzebny do usuwania zduplikowanych wpisów w liście
+    # hash dodany po zdefiniowaniu __eq__ domyślny nie działał, potrzebny do usuwania zduplikowanych wpisów w liście
     def __hash__(self):
-        return hash(self.title)+hash(self.year)+hash(self.genre)+hash(self.episode)+hash(self.season)
+        return (
+            hash(self.title)
+            + hash(self.year)
+            + hash(self.genre)
+            + hash(self.episode)
+            + hash(self.season)
+        )
 
     def __eq__(self, other):
-        return all((self.title == other.title, self.year == other.year, self.genre == other.genre, self.episode == other.episode, self.season == other.season))
+        return all(
+            (
+                self.title == other.title,
+                self.year == other.year,
+                self.genre == other.genre,
+                self.episode == other.episode,
+                self.season == other.season,
+            )
+        )
 
     def episode_number(series_name):
         x = search(series_name)
         return len(x)
-# films_catalogue = []
 
 
-"""
-def add_to_list(i):
-    #    try:
-    #        x = films_catalogue
-    #    except NameError:
-    #        films_catalogue = []
-    #        return films_catalogue
-    if isinstance(i, Films):
-        films_catalogue.append(i)
-#        print('Added to list', i)
-    else:
-        print('NOT a film')
-    return films_catalogue
-"""
-
-
-def split_films():
+def split_films(catalogue):
     movies_list = []
     series_list = []
-    for i in films_catalogue:
+    for i in catalogue:
         if type(i) == Films:
             movies_list.append(i)
         else:
             series_list.append(i)
-    return sorted(movies_list, key=lambda movie: movie.title), sorted(series_list, key=lambda movie: movie.title)
+    return sorted(movies_list, key=lambda movie: movie.title), sorted(
+        series_list, key=lambda movie: movie.title
+    )
 
 
-def get_movies():
-    x = split_films()
+def get_movies(catalogue):
+    x = split_films(catalogue)
     return x[0]
 
 
-def get_series():
-    x = split_films()
+def get_series(catalogue):
+    x = split_films(catalogue)
     return x[1]
 
 
-def search(film):
+def search(film, catalogue):
     search_list = []
-    for i in films_catalogue:
+    for i in catalogue:
         if i.title == film:
             search_list.append(i)
     return search_list
 
 
-def search_serie(serie, season=1):
+def search_serie(serie, catalogue, season=1):
     search_list = []
-    for i in films_catalogue:
+    for i in catalogue:
         if i.title == serie and i.season == season:
             search_list.append(i)
     return search_list
 
 
-def generate_views():
-    x = films_catalogue[random.randint(0, len(films_catalogue)-1)]
+def generate_views(catalogue):
+    x = catalogue[random.randint(0, len(films_catalogue) - 1)]
     return x.play(random.randint(1, 100))
 
 
-def generate_views_10():
+def generate_views_10(catalogue):
     i = 0
     while i <= 10:
         i += 1
-        generate_views()
+        generate_views(catalogue)
 
 
-def top_titles(number, content_type='Any'):
-    x = split_films()
+def top_titles(number, catalogue, content_type="Any"):
+    x = split_films(catalogue)
     if content_type == Films:
-        sorted_cat = sorted(
-            x[0], key=lambda film: film.views, reverse=True)
+        sorted_cat = sorted(x[0], key=lambda film: film.views, reverse=True)
     elif content_type == Series:
         sorted_cat = sorted(x[1], key=lambda film: film.views, reverse=True)
     else:
-        sorted_cat = sorted(
-            films_catalogue, key=lambda film: film.views, reverse=True)
+        sorted_cat = sorted(films_catalogue, key=lambda film: film.views, reverse=True)
     return sorted_cat[:number]
 
 
-def add_season(serie, season, episodes, year, genre):
-    for i in range(1, episodes+1):
-        films_catalogue.append(Series(i, season, serie, year, genre))
-    return films_catalogue
+def add_season(serie, season, episodes, year, genre, catalogue):
+    for i in range(1, episodes + 1):
+        catalogue.append(Series(i, season, serie, year, genre))
+    return catalogue
 
 
-def number_of_episodes(serie, season=0):
+def number_of_episodes(serie, catalogue, season=0):
     # podaje ilość odcinków dla podanego sezonu serialu, albo ilość odcinków wszystkich sezonów
     if season == 0:
-        if isinstance(search(serie)[0], Series):
-            return len(search(serie))
+        if isinstance(search(serie, catalogue)[0], Series):
+            return len(search(serie, catalogue))
         else:
             return f"{serie} nie jest serialem"
-    elif isinstance(search_serie(serie, season)[0], Series):
-        return len(search_serie(serie, season))
+    elif isinstance(search_serie(serie, catalogue, season)[0], Series):
+        return len(search_serie(serie, catalogue, season))
     else:
         return f"{serie} nie jest serialem"
 
@@ -169,78 +170,64 @@ def catalogue_cleaner(list_name):
 
 
 if __name__ == "__main__":
+    films_catalogue = [Films("Chłopaki nie płaczą", 2000, "comedy")]
 
-    import random
-    from datetime import date
-    today = date.today()
-    films_catalogue = [Films("Chłopaki nie płaczą", 2000, 'comedy')]
-
-    add_season("Firefly", 1, 15, 2002, "S-F")
-    add_season("Monty Python's Flying Circus", 1, 13, 1969, 'comedy')
-    add_season("Monty Python's Flying Circus", 2, 13, 1970, 'comedy')
-    add_season("Monty Python's Flying Circus", 3, 13, 1972, 'comedy')
-    add_season("Monty Python's Flying Circus", 4, 6, 1974, 'comedy')
-    add_season("M.A.S.H.", 1, 24, 1972, 'comedy')
-    add_season("M.A.S.H.", 2, 24, 1973, 'comedy')
-    films_catalogue.append(Films('Hellraiser', 1987, 'horror'))
+    add_season("Firefly", 1, 15, 2002, "S-F", films_catalogue)
+    add_season("Monty Python's Flying Circus", 1, 13, 1969, "comedy", films_catalogue)
+    add_season("Monty Python's Flying Circus", 2, 13, 1970, "comedy", films_catalogue)
+    add_season("Monty Python's Flying Circus", 3, 13, 1972, "comedy", films_catalogue)
+    add_season("Monty Python's Flying Circus", 4, 6, 1974, "comedy", films_catalogue)
+    add_season("M.A.S.H.", 1, 24, 1972, "comedy", films_catalogue)
+    add_season("M.A.S.H.", 2, 24, 1973, "comedy", films_catalogue)
+    films_catalogue.append(Films("Hellraiser", 1987, "horror"))
     films_catalogue.append(Films("A Beautiful Mind", 2001, "drama"))
-    films_catalogue.append(Films("The Thing", 1982, 'horror'))
-    films_catalogue.append(Films("The Thing", 2011, 'horror'))
-    films_catalogue.append(Films('It', 1989, 'horror'))
-    films_catalogue.append(Series(1, 3, 'The Simpsons', 2000, 'comedy'))
-    films_catalogue.append(Series(2, 3, 'The Simpsons', 2000, 'comedy'))
-    films_catalogue.append(Series(1, 1, 'M.A.S.H.', 1972, 'comedy'))
-    films_catalogue.append(Films("Pulp Fiction", 1994, 'crime'))
-    films_catalogue.append(Films('Life of Brian', 1979, 'comedy'))
-    films_catalogue.append(Series(1, 1, "Star Trek", 1999, 'S-F'))
+    films_catalogue.append(Films("The Thing", 1982, "horror"))
+    films_catalogue.append(Films("The Thing", 2011, "horror"))
+    films_catalogue.append(Films("It", 1989, "horror"))
+    films_catalogue.append(Series(1, 3, "The Simpsons", 2000, "comedy"))
+    films_catalogue.append(Series(2, 3, "The Simpsons", 2000, "comedy"))
+    films_catalogue.append(Series(1, 1, "M.A.S.H.", 1972, "comedy"))
+    films_catalogue.append(Films("Pulp Fiction", 1994, "crime"))
+    films_catalogue.append(Films("Life of Brian", 1979, "comedy"))
+    films_catalogue.append(Series(1, 1, "Star Trek", 1999, "S-F"))
     films_catalogue.append(Films("Blade Runner", 1982, "S-F"))
     films_catalogue.append(Films("Blade Runner 2049", 2017, "S-F"))
-    films_catalogue.append(Films('It', 1989, 'horror'))
+    films_catalogue.append(Films("It", 1989, "horror"))
     # print(len(films_catalogue))
     # films_catalogue = list(dict.fromkeys(films_catalogue))
     films_catalogue = catalogue_cleaner(films_catalogue)
     # print(len(films_catalogue))
-    generate_views_10()
+    generate_views_10(films_catalogue)
     # for i in films_catalogue:
     #    print(i, i.views)
 
-    print("Biblioteka filmów %d rekordów \nNajpopularniejsze w dniu %d.%02d.%d \nfilmy: %s \nseriale: %s" %
-          (len(films_catalogue), today.day, today.month, today.year, top_titles(3, Films), top_titles(3, Series)))
+    print(
+        "Biblioteka filmów %d rekordów \nNajpopularniejsze w dniu %d.%02d.%d \nfilmy: %s \nseriale: %s"
+        % (
+            len(films_catalogue),
+            today.day,
+            today.month,
+            today.year,
+            top_titles(3, films_catalogue, Films),
+            top_titles(3, films_catalogue, Series),
+        )
+    )
+"""
+    print(
+        top_titles(1, films_catalogue, Series),
+        top_titles(1, films_catalogue, Series)[0].views,
+    )
+    print(
+        "Ilość odcinków 'M.A.S.H': %d"
+        % (number_of_episodes("M.A.S.H.", films_catalogue))
+    )
 
-    """
-    print(top_titles(1, Series), top_titles(1, Series)[0].views)
-    print("Ilość odcinków 'M.A.S.H': %d" % (episode_number('M.A.S.H.')))
-    # films_catalogue = list(set(films_catalogue))
-    print(len(search('M.A.S.H.')))
+    print(number_of_episodes("M.A.S.H.", films_catalogue, 2))
+    print(number_of_episodes("It", films_catalogue))
 
-    it_list = []
-    it_list.append(Films("It", 1989, "horror"))
-    it_list.append(Films("It", 1989, "horror"))
-    print(it_list[0] == it_list[1])
-    simpson_list = []
-    simpson_list.append(Series(1, 3, 'The Simpsons', 2000, 'comedy'))
-    simpson_list.append(Series(1, 3, 'The Simpsons', 2000, 'comedy'))
-    simpson_list.append(Series(2, 3, 'The Simpsons', 2000, 'comedy'))
-    print(simpson_list[0] == simpson_list[1])
-    print(simpson_list[0] == simpson_list[2])
-    print(type(simpson_list[0]), type(it_list[0]))
-    print(list(dict.fromkeys(it_list)))
-    print(hash(simpson_list[0]))
+    print(search_serie("The Simpsons", films_catalogue, 3))
+    print(top_titles(5, films_catalogue))
 
-    print(number_of_episodes('M.A.S.H.'))
-    print(number_of_episodes('M.A.S.H.', 2))
-    print(number_of_episodes('It'))
-    print(number_of_episodes("Monty Python's Flying Circus", 1))
-    print(number_of_episodes("Monty Python's Flying Circus", 4))
-
-    print(search_serie('The Simpsons', 3))
-    print(top_titles(5))
-    
-    print(number_of_episodes('It'))
-    print(number_of_episodes('M.A.S.H.'))
-    print(number_of_episodes('M.A.S.H.', 2))
-    print(type(search('M.A.S.H.')[0]) == Series)
-    print(get_movies())
-    """
-    # print("!!!!!!!!!!!!!!!!" '\n', type(films_catalogue[0]))
-    # print("GLOBALNE!!!!!!!!!!!: " '\n', globals())
+    print(type(search("M.A.S.H.", films_catalogue)[0]) == Series)
+    print(get_movies(films_catalogue))
+"""
